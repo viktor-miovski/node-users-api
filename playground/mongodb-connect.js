dc8f8d69c47848a496740764d952c5c3cf81335d
viktor-miovski <viktormiovski@gmail.com>
2018-11-25 12:01:56 +0200

    Add playground folder with basic node code lines to connect,insert to db and fetch data from db

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,18 @@
+// const MongoClient = require("mongodb").MongoClient;
+const { MongoClient, ObjectID } = require("mongodb");
+
+MongoClient.connect('mongodb://localhost:27017/AppDb', (err, client) => {
+    if (err) { return console.log("Unable to connect to the server") };
+    console.log("Connected to the server");
+    const db = client.db('AppDb')
+
+    db.collection('Todos').insertOne({
+        text: 'Something to do',
+        completed: false
+    }, (err, result) => {
+        if (err) { return console.log('Unable to insert todo: ', err) }
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    })
+    
+    client.close();
+});
