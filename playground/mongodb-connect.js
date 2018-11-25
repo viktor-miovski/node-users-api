7b5d9f5833fe2f37daa000890f4b0afb81af6af7
viktor-miovski <viktormiovski@gmail.com>
2018-11-25 13:21:19 +0200

    Add update script

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -7,28 +7,28 @@ MongoClient.connect('mongodb://localhost:27017', (err, client) => {
     const db = client.db('AppDb')
 
     //Insert many documents to Todos collection inside AppDB database
-    db.collection('Todos').insertMany([
-        {
-            text: 'Something to do',
-            completed: false
-        },
-        {
-            text: 'Something to do',
-            completed: false
-        }
-    ], (err, result) => {
-        if (err) { return console.log('Unable to insert in Todos: ', err) }
-        console.log(JSON.stringify(result.ops, undefined, 2));
-    })
-
-    //Insert one document to Todos collection inside AppDB database
-    // db.collection('Todos').insertOne({
-    //     text: 'Something to do',
-    //     completed: false
-    // }, (err, result) => {
-    //     if (err) { return console.log('Unable to insert todo: ', err) }
+    // db.collection('Todos').insertMany([
+    //     {
+    //         text: 'Something to do',
+    //         completed: false
+    //     },
+    //     {
+    //         text: 'Something to do',
+    //         completed: false
+    //     }
+    // ], (err, result) => {
+    //     if (err) { return console.log('Unable to insert in Todos: ', err) }
     //     console.log(JSON.stringify(result.ops, undefined, 2));
     // })
+
+    //Insert one document to Todos collection inside AppDB database
+    db.collection('Todos').insertOne({
+        text: 'Something to do',
+        completed: false
+    }, (err, result) => {
+        if (err) { return console.log('Unable to insert todo: ', err) }
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    })
 
     client.close();
 });
